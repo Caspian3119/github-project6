519be3fe05d45d17164b0812ac10aaabd8a6cbf0
Caspian del Mundo <caspianjames2@gmail.com>
2022-10-11 08:25:18 +0800

    feat: add login and register page

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -1,50 +1,68 @@
-import React,{useState, useRef} from 'react'
-import Pin from '@mui/icons-material/LocationOn'
-import Close from '@mui/icons-material/Close';
-import './register.css'
-import axios from 'axios'
+import React, { useState, useRef } from "react";
+import Pin from "@mui/icons-material/LocationOn";
+import "./register.css";
+import axios from "axios";
 
-const Register = ({setShowRegister}) => {
-const [success, setSuccess] = useState(false)
-const [failed, setFailed] = useState(false)
-const name = useRef()
-const email = useRef()
-const password = useRef()
+const Register = ({ setShowRegister, setShowLogin }) => {
+  const [success, setSuccess] = useState(false);
+  const [failed, setFailed] = useState(false);
+  const name = useRef();
+  const email = useRef();
+  const password = useRef();
 
-const handleSubmit = async (e) =>{
+  const showForm = () => {
+    setShowRegister(false);
+    setShowLogin(true);
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const newUser = {
-        username: name.current.value,
-        email: email.current.value,
-        password: password.current.value
+      username: name.current.value,
+      email: email.current.value,
+      password: password.current.value,
+    };
+    if (newUser.username == "") {
     }
-
-    try{
-        axios.post('http://localhost:3000/api/users/register', newUser)
-        setSuccess(true)
-        setFailed(false)
+    try {
+      axios.post("http://localhost:3000/api/users/register", newUser);
+      setSuccess(true);
+      setFailed(false);
+    } catch (err) {
+      setFailed(err);
     }
-    catch(err){
-        setFailed(err)
-    }
-}
+  };
   return (
-    <div className = "regContainer">
-    <div className='logo'>
-        <Pin/>
-        Pin-N-Rate
+    <div className="container">
+      <div className="regContainer">
+        <div className="logo">
+          <Pin />
+          <p className="title">Pin-N-Rate</p>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <label>Username</label>
+          <input type="text" placeholder="username" ref={name}></input>
+          <label>Email</label>
+          <input type="email" placeholder="email" ref={email}></input>
+          <label>Password</label>
+          <input type="password" placeholder="password" ref={password}></input>
+          <button className="regButton">Register</button>
+          {success && (
+            <div className="successMsg">
+              <span className="success">Account Successfully Created</span>
+              <br />
+              <button
+                className="regButton"
+                onClick={() => setShowRegister(showForm)}
+              >
+                Back to login
+              </button>
+            </div>
+          )}
+          {failed && <span className="fail">Something went wrong</span>}
+        </form>
+      </div>
     </div>
-    <form onSubmit={handleSubmit}>
-        <input type = "text" placeholder = "username" ref = {name}></input>
-        <input type = "email" placeholder = "email" ref = {email}></input>
-        <input type = "password" placeholder = "password" ref = {password}></input>
-        <button className="regButton">Register</button>
-        {success &&  <span className="success">Account Created</span>}
-        {failed &&  <span className="fail">Something went wrong</span>}   
-    </form>
-    <Close className = "close" onClick = {() => setShowRegister(false)}/>
-</div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
